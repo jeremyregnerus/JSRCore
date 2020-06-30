aaaa6a936c1e585d3c68bec29824d1c64da45c0c
Jeremy Regnerus <jeremy_regnerus@msn.com>
2020-06-30 10:46:05 -0400

    Added assembly info

--- JSR.Converters/Properties/AssemblyInfo.cs
+++ JSR.Converters/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Converters")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Jeremy Regnerus")]
 [assembly: AssemblyProduct("Converters")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyCopyright("Copyright © Jeremy Regnerus 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
