31b05c06f9d322bcfe0654019a5adc595c17ff4d
Jeremy Regnerus <jeremy_regnerus@msn.com>
2020-06-30 11:15:43 -0400

    Added IRequestViewClose interface and ViewModelBase object.

---
+++ JSR.BaseClassLibrary/IRequestViewClose.cs
@@ -0,0 +1,21 @@
+﻿// <copyright file="IRequestViewClose.cs" company="Jeremy Regnerus">
+// Copyright (c) Jeremy Regnerus. All rights reserved.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JSR.BaseClassLibrary
+{
+    /// <summary>
+    /// Interface for requesting closing a viewmodel.
+    /// </summary>
+    public interface IRequestViewClose
+    {
+        /// <summary>
+        /// Event to request closing a ViewModel within an interface.
+        /// </summary>
+        event EventHandler RequestViewCloseEvent;
+    }
+}
---
+++ JSR.BaseClassLibrary/ViewModelBase.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="ViewModelBase.cs" company="Jeremy Regnerus">
+// Copyright (c) Jeremy Regnerus. All rights reserved.
+// </copyright>
+
+using System;
+
+namespace JSR.BaseClassLibrary
+{
+    /// <summary>
+    /// Base ViewModel object for MVVM environments.
+    /// </summary>
+    public class ViewModelBase : BaseClass, IRequestViewClose
+    {
+        private DelegateCommand requestViewCloseCommand;
+
+        /// <inheritdoc/>
+        public event EventHandler RequestViewCloseEvent;
+
+        /// <summary>
+        /// Gets a command to request the view to close.
+        /// </summary>
+        public DelegateCommand RequestCloseCommand => requestViewCloseCommand ?? (requestViewCloseCommand = new DelegateCommand(RequestViewClose));
+
+        /// <summary>
+        /// Request closing this object's ViewModel.
+        /// </summary>
+        protected void RequestViewClose()
+        {
+            RequestViewCloseEvent?.Invoke(this, new EventArgs());
+        }
+    }
+}
