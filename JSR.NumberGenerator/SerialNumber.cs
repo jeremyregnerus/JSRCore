7f601480cbe40297533b287788751641802fd7fc
Jeremy Regnerus <jeremy_regnerus@msn.com>
2021-07-08 13:42:21 -0400

    Added increment methods, formatted and raw serial number methods.

--- JSR.NumberGenerator/SerialNumber.cs
+++ JSR.NumberGenerator/SerialNumber.cs
@@ -37,20 +37,59 @@ namespace JSR.NumberGenerator
         public const int SecondsInADay = HoursInADay * SecondsInAnHour;
 
         /// <summary>
-        /// Generate a serial number for the current <see cref="DateTime"/>.
+        /// Generate a formatted serial number for the current <see cref="DateTime"/>.
         /// </summary>
         /// <returns>A 12 digit serial number seperated by a hyphen (XXXXXX-XXXXXX).</returns>
-        public static string GetSerialNumber()
+        public static string GetFormattedSerialNumber()
         {
-            return GetSerialNumber(DateTime.UtcNow);
+            return GetFormattedSerialNumber(DateTime.UtcNow);
         }
 
         /// <summary>
-        /// Generate a serial number for the provided <see cref="DateTime"/>.
+        /// Generate a formatted serial number.
         /// </summary>
-        /// <param name="date"><see cref="DateTime"/> to generate a serial number from.</param>
+        /// <param name="date"><see cref="DateTime"/> to generate the serial number from.</param>
         /// <returns>A 12 digit serial number, separated by a hyphen (XXXXXX-XXXXXX).</returns>
-        public static string GetSerialNumber(DateTime date)
+        public static string GetFormattedSerialNumber(DateTime date)
+        {
+            return GetFormattedSerialNumber(GetSerialNumber(date));
+        }
+
+        /// <summary>
+        /// Generate a formatted serial number.
+        /// </summary>
+        /// <param name="serialNumber">A 12 digit <see cref="long"/> value serial number to format.</param>
+        /// <returns>A 12 digit serial number, separated by a hyphen (XXXXXX-XXXXXX).</returns>
+        public static string GetFormattedSerialNumber(long serialNumber)
+        {
+            return serialNumber.ToString().Insert(serialNumber.ToString().Length - 5, "-");
+        }
+
+        /// <summary>
+        /// Increments a serial number.
+        /// </summary>
+        /// <param name="serialNumber">A formatted (XXXXXX-XXXXXX) <see cref="string"/> as a serial number to increment.</param>
+        /// <returns>A 12 digit serial number, separated by a hyphen (XXXXXX-XXXXXX) incremented a value of 1 from the provided value.</returns>
+        public static string IncrementFormattedSerialNumber(string serialNumber)
+        {
+            return GetFormattedSerialNumber(IncrementSerialNumber(GetSerialNumber(serialNumber)));
+        }
+
+        /// <summary>
+        /// Generate a serial number as a long value for the current DateTime.
+        /// </summary>
+        /// <returns>A 12 digit long value serial number.</returns>
+        public static long GetSerialNumber()
+        {
+            return GetSerialNumber(DateTime.Now);
+        }
+
+        /// <summary>
+        /// Generate a serial number as a long value.
+        /// </summary>
+        /// <param name="date"><see cref="DateTime"/> to gernate the serial number from.</param>
+        /// <returns>12 digit long value serial number.</returns>
+        public static long GetSerialNumber(DateTime date)
         {
             long year = (date.Year % 1000) * 100000000;
             long day = (date.DayOfYear - 1) * SecondsInADay;
@@ -58,11 +97,41 @@ namespace JSR.NumberGenerator
             long minute = date.Minute * SecondsInAMinute;
             long second = date.Second;
 
-            long secondOfYear = year + day + hour + minute + second;
+            return year + day + hour + minute + second;
+        }
 
-            string serialNumber = secondOfYear.ToString();
+        /// <summary>
+        /// Gets a serial number long value from a formatted string.
+        /// </summary>
+        /// <param name="serialNumber">A formatted ("XXXXXX-XXXXXX") <see cref="string"/> value representing a serial number.</param>
+        /// <returns>12 digit long value serial number.</returns>
+        public static long GetSerialNumber(string serialNumber)
+        {
+            if (serialNumber.Length != 13 || !serialNumber.Contains("-"))
+            {
+                throw new ArgumentOutOfRangeException($"The value {serialNumber} is not a valid formatted serial number.");
+            }
 
-            return serialNumber.Insert(serialNumber.Length - 5, "-");
+            serialNumber = serialNumber.Replace("-", string.Empty);
+
+            if (long.TryParse(serialNumber, out long result))
+            {
+                return result;
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException($"The value {serialNumber} is not a valid formatted serial number.");
+            }
+        }
+
+        /// <summary>
+        /// Increments a serial number by 1.
+        /// </summary>
+        /// <param name="serialNumber">A 12 digit serial number.</param>
+        /// <returns>A 12 digit serial number incremented by 1.</returns>
+        public static long IncrementSerialNumber(long serialNumber)
+        {
+            return serialNumber++;
         }
     }
 }
