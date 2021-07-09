f6a3a9861b27e7e6df4c347206b993fc6901b43d
Jeremy Regnerus <jeremy_regnerus@msn.com>
2021-07-09 15:02:11 -0400

    Updated analyzers....I think

--- JSR.Converters/Properties/AssemblyInfo.cs
+++ JSR.Converters/Properties/AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jeremy Regnerus")]
 [assembly: AssemblyProduct("JSR.Converters")]
-[assembly: AssemblyCopyright("Copyright Jeremy Regnerus ©  2020")]
+[assembly: AssemblyCopyright("Copyright Jeremy Regnerus © 2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -36,5 +36,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
