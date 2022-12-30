3a200df852bd6da93b533cb8522e3245811d535d
Jeremy Regnerus <jeremy_regnerus@msn.com>
2022-12-30 15:19:21 -0500

    created INotifyPropertyChanged base class

---
+++ JSR.BaseClasses/NotifyPropertyChanged.cs
@@ -0,0 +1,90 @@
+﻿// <copyright file="NotifyPropertyChanged.cs" company="Jeremy Regnerus">
+// Copyright (c) Jeremy Regnerus. All rights reserved.
+// </copyright>
+
+using System.ComponentModel;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+namespace JSR.BaseClasses
+{
+    /// <summary>
+    /// Base implementation of <see cref="INotifyPropertyChanged"/>.
+    /// </summary>
+    public abstract class NotifyPropertyChanged : INotifyPropertyChanged
+    {
+        /// <inheritdoc/>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Raises the <see cref="PropertyChangedEventHandler"/> for ever property within the object.
+        /// </summary>
+        protected void NotifyAllPropertiesChanged()
+        {
+            foreach (PropertyInfo property in GetType().GetProperties())
+            {
+                PropertyChangedNotify(property.Name);
+            }
+        }
+
+        /// <summary>
+        /// Raise the <see cref="PropertyChangedEventHandler"/> for multiple properties.
+        /// </summary>
+        /// <param name="propertyNames">Array of property names to raise the event handler.</param>
+        protected void NotifyPropertiesChanged(string[] propertyNames)
+        {
+            if (propertyNames != null)
+            {
+                foreach (string propertyName in propertyNames)
+                {
+                    PropertyChangedNotify(propertyName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Raise the <see cref="PropertyChangedEventHandler"/> for multiple properties.
+        /// </summary>
+        /// <param name="propertyNames">List of property names to raise the event handlder.</param>
+        protected void NotifyPropertiesChanged(List<string> propertyNames)
+        {
+            if (propertyNames != null)
+            {
+                foreach (string propertyName in propertyNames)
+                {
+                    PropertyChangedNotify(propertyName);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Raise the <see cref="PropertyChangedEventHandler"/>.
+        /// </summary>
+        /// <param name="propertyName">Property name to raise the event handler.</param>
+        protected void PropertyChangedNotify([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// Sets the value for a property.
+        /// </summary>
+        /// <typeparam name="T">Type of value to set.</typeparam>
+        /// <param name="field">Field storing the value for the property.</param>
+        /// <param name="value">Value to assign to the property.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <returns>True if the value was changed. This will return false if the values are the same.</returns>
+        protected virtual bool SetValue<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
+        {
+            if (EqualityComparer<T>.Default.Equals(value, field))
+            {
+                return false;
+            }
+
+            field = value;
+            PropertyChangedNotify(propertyName);
+
+            return true;
+        }
+    }
+}
