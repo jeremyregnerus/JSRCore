52700d90c237909a680429c3ce2c782983e24b20
Jeremy Regnerus <jeremy_regnerus@msn.com>
2022-12-31 12:25:34 -0500

    created tests for BaseNotifyPropertyChanged

---
+++ JSR.BaseClasses.Tests/BaseNotifyPropertyChangedTests.cs
@@ -0,0 +1,33 @@
+﻿// <copyright file="BaseNotifyPropertyChangedTests.cs" company="Jeremy Regnerus">
+// Copyright (c) Jeremy Regnerus. All rights reserved.
+// </copyright>
+
+using System.Diagnostics.CodeAnalysis;
+using JSR.Asserts;
+using JSR.BaseClasses.Tests.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JSR.BaseClasses.Tests
+{
+    [TestClass]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Unit tests.")]
+    [SuppressMessage("Naming", "CA1707:Identifiers should not contain underscores", Justification = "Unit tests")]
+    public class BaseNotifyPropertyChangedTests
+    {
+        [TestMethod]
+        [DataRow(nameof(MockBaseNotifyPropertyChanged.StringProperty))]
+        [DataRow(nameof(MockBaseNotifyPropertyChanged.IntegerProperty))]
+        public void SetValue_ChangesPropertyValues(string propertyName)
+        {
+            Assert.That.PropertyChangesValue<MockBaseNotifyPropertyChanged>(propertyName);
+        }
+
+        [TestMethod]
+        [DataRow(nameof(MockBaseNotifyPropertyChanged.StringProperty))]
+        [DataRow(nameof(MockBaseNotifyPropertyChanged.IntegerProperty))]
+        public void NotifiesPropertyChangedWhenChanged(string propertyName)
+        {
+            Assert.That.NotifiesPropertyChanged<MockBaseNotifyPropertyChanged>(propertyName);
+        }
+    }
+}
---
+++ JSR.BaseClasses.Tests/Mocks/MockBaseNotifyPropertyChanged.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="MockBaseNotifyPropertyChanged.cs" company="Jeremy Regnerus">
+// Copyright (c) Jeremy Regnerus. All rights reserved.
+// </copyright>
+
+namespace JSR.BaseClasses.Tests.Mocks
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "Mock class")]
+    public class MockBaseNotifyPropertyChanged : BaseNotifyPropertyChanged
+    {
+        private string stringProperty = string.Empty;
+
+        private int intProperty;
+
+        public string StringProperty { get => stringProperty; set => SetValue(ref stringProperty, value); }
+
+        public int IntegerProperty { get => intProperty; set => SetValue(ref intProperty, value); }
+    }
+}
