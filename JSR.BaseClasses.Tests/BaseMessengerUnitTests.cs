78c1739ef94b9d7589f23cc33f11913eb2f4aa82
Jeremy Regnerus <jeremy_regnerus@msn.com>
2023-01-02 17:38:05 -0500

    added two more tests for issue #4

--- JSR.BaseClasses.Tests/BaseMessengerUnitTests.cs
+++ JSR.BaseClasses.Tests/BaseMessengerUnitTests.cs
@@ -68,22 +68,55 @@ namespace JSR.BaseClasses.Tests
         [TestMethod]
         public void Message_DoesNotRaiseNotificationWhenChildMessagesMatch()
         {
-            MockBaseMessengerParent messenger = new();
+            MockBaseMessengerParent parent = new();
             string message = new Random().NextString(8);
-            messenger.Child.ChangeMessage(message);
+            parent.Child.ChangeMessage(message);
 
-            Assert.AreEqual(message, messenger.Message);
+            Assert.AreEqual(message, parent.Message);
 
             List<string> messages = new();
 
-            messenger.OnMessage += (sender, message) => messages.Add(message);
+            parent.OnMessage += (sender, message) => messages.Add(message);
 
             for (int i = 0; i < new Random().Next(5, 20); i++)
             {
-                messenger.Child.ChangeMessage(message);
+                parent.Child.ChangeMessage(message);
             }
 
             Assert.AreEqual(0, messages.Count);
+        }
+
+        [TestMethod]
+        public void Message_UpdatesForNewChildren()
+        {
+            MockBaseMessengerParent parent = new();
+            List<string> messages = new();
+            parent.OnMessage += (sender, message) => messages.Add(message);
+
+            MockBaseMessenger child = new();
+            parent.Child = child;
+
+            string message = new Random().NextString();
+
+            child.ChangeMessage(message);
+
+            Assert.AreEqual(message, parent.Message);
+        }
+
+        [TestMethod]
+        public void Message_DoesNotUpdateForOldChildren()
+        {
+            MockBaseMessengerParent parent = new();
+            List<string> messages = new();
+            parent.OnMessage += (sender, message) => messages.Add(message);
+
+            MockBaseMessenger oldChild = parent.Child;
+            parent.Child = new();
+
+            string message = new Random().NextString();
+            oldChild.ChangeMessage(message);
+
+            Assert.AreNotEqual(message, parent.Message);
         }
     }
 }
