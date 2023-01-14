3962ca40c394f9fbb13d3924942d9a8060921b64
Jeremy Regnerus <jeremy_regnerus@msn.com>
2023-01-14 13:08:02 -0500

    Changed DelegateCommands to RelayCommands

--- JSR.BaseClasses/RelayCommand.cs
+++ JSR.BaseClasses/RelayCommand.cs
@@ -5,27 +5,27 @@ namespace JSR.BaseClasses
     /// <summary>
     /// Provides a default implementation of the <see cref="ICommand"/> interface for use within MVVM and WPF.
     /// </summary>
-    public class DelegateCommand : ICommand
+    public class RelayCommand : ICommand
     {
         private readonly Action execute;
         private readonly Func<bool>? canExecute;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DelegateCommand"/> class.
+        /// Initializes a new instance of the <see cref="RelayCommand"/> class.
         /// </summary>
-        /// <param name="exectute">Action to execute.</param>
-        public DelegateCommand(Action exectute) : this(exectute, null)
+        /// <param name="execute">Action to execute.</param>
+        public RelayCommand(Action execute)
         {
+            this.execute = execute;
         }
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="DelegateCommand"/> class.
+        /// Initializes a new instance of the <see cref="RelayCommand"/> class.
         /// </summary>
         /// <param name="execute">Action to execute.</param>
         /// <param name="canExecute">Function specifying if this action can execute when called.</param>
-        public DelegateCommand(Action execute, Func<bool>? canExecute)
+        public RelayCommand(Action execute, Func<bool>? canExecute) : this(execute)
         {
-            this.execute = execute;
             this.canExecute = canExecute;
 
             if (this.canExecute != null)
@@ -35,6 +35,16 @@ namespace JSR.BaseClasses
         }
 
         /// <inheritdoc/>
+        /// <remarks>
+        /// There are many examples where CanExectute is handled by CommandManager using the following
+        ///
+        /// {
+        ///    add { CommandManager.RequerySuggested += value; }
+        ///    remove { CommandManager.RequerySuggested -= value; }
+        /// }
+        ///
+        /// This may need to be implemented after, as this class library is not flagged for WPF or Windows.
+        /// </remarks>
         public event EventHandler? CanExecuteChanged;
 
         /// <inheritdoc/>
